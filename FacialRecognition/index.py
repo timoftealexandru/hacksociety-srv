3b7c8bef677a47170a28fef041d58ef73c2d7937
Nicu <timofte.alexandru05@yahoo.com>
2017-10-29 10:43:25 +0200

    solved undiscovered faces problem

--- FacialRecognition/index.py
+++ FacialRecognition/index.py
@@ -22,22 +22,24 @@ def index():
 
 @app.route('/compare', methods=['GET'])
 def stopVideo():
-		picture_of_me = face_recognition.load_image_file("me2.jpg")
-		my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+    picture_of_me = face_recognition.load_image_file("me2.jpg")
+    my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
 
-		urllib.request.urlretrieve("http://localhost:5000/base/img.jpg", "img.jpg")
+    urllib.request.urlretrieve("http://10.5.5.46:5000/base/img.jpg", "img.jpg")
 
-		unknown_picture = face_recognition.load_image_file("img.jpg")
-		unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
-		
-		
-		results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
-		
-		if results[0] == True:
-			print("It's a picture of me!")
-		else:
-			print("It's not a picture of me!")
-		return str(results[0])
+    unknown_picture = face_recognition.load_image_file("img.jpg")
+    unknown_face_encoding = face_recognition.face_encodings(unknown_picture)
+
+    if not len(unknown_face_encoding) > 0:
+        return "False"
+
+    results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding[0])
+
+    if results[0] == True:
+        print("It's a picture of me!")
+    else:
+        print("It's not a picture of me!")
+    return str(results[0])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001 ,debug=False)
