00656442c85c5fec261fb7c5c4a3e7aee4ac0fc4
Tudor <stupariu.tudorf@gmail.com>
2017-10-29 11:13:54 +0200

    Multiple file checks

--- FacialRecognition/index.py
+++ FacialRecognition/index.py
@@ -15,7 +15,9 @@ import random
 import os
 import sys
 sys.path.append(os.getcwd())
-#import face_recognition
+import face_recognition
+
+pictures = [{'file' : 'notme.jpg', 'name' : 'Cristi'},{'file' : 'me2.jpg', 'name' : 'Tudor'},{'file' : 'me.jpg', 'name' : 'Tudor'}]
 
 @app.route('/', methods=['GET'])
 def index():
@@ -27,24 +29,28 @@ def getStaticImage(filename):
 
 @app.route('/compare', methods=['GET'])
 def stopVideo():
-    picture_of_me = face_recognition.load_image_file("me2.jpg")
-    my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+	found = False
+	foundItem = {'file' : "False", 'name' : "False"}
+	for i in range(len(pictures)):
+ 		picture_of_me = face_recognition.load_image_file(pictures[i]['file'])
+ 		my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+ 		urllib.request.urlretrieve("http://10.5.5.46:5000/base/img.jpg", "img.jpg")
+	 	unknown_picture = face_recognition.load_image_file('img.jpg')
+	 	unknown_face_encoding = face_recognition.face_encodings(unknown_picture)
+	 	err = False
+	 	if not (len(unknown_face_encoding) > 0):
+	 		err = True
+	 	if not err:
+	 		results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding[0])
+	 		if results[0] == True:
+	 			found = True
+	 			foundItem = pictures[i]
 
-    urllib.request.urlretrieve("http://10.5.5.46:5000/base/img.jpg", "img.jpg")
-
-    unknown_picture = face_recognition.load_image_file("img.jpg")
-    unknown_face_encoding = face_recognition.face_encodings(unknown_picture)
-
-    if not len(unknown_face_encoding) > 0:
-        return "False"
-
-    results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding[0])
-
-    if results[0] == True:
-        print("It's a picture of me!")
-    else:
-        print("It's not a picture of me!")
-    return str(results[0])
+	if found == True:
+	 	print("It's a picture of me!")
+	else:
+		print("It's not a picture of me!")
+	return Response(json.dumps(foundItem), 200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001 ,debug=False)
